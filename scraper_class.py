6e8f82fdde4734ac3a205e11fe46d74c580e3e32
Dennis Yang <dennisyang3.14@gmail.com>
2022-11-18 21:28:07 +0000

    Scrapes all coin links

--- scraper_class.py
+++ scraper_class.py
@@ -1,10 +1,15 @@
 # %%
-from selenium import webdriver 
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
 import time
 
+
 driver = webdriver.Chrome()
-URL = "https://www.xe.com/currencyconverter/convert/?Amount=1&From=GBP&To=USD"
+URL = "https://crypto.com/price/ethereum"
 website = driver.get(URL)
 
 
@@ -13,22 +18,43 @@ class ScraperClass:
         self.website = website
 
     def cookie_ad_clicker(self):
-        time_now  = time.time()
+        delay = 10
         try:    
-            accept_cookies_button = driver.find_element(by=By.XPATH, value='//*[@class="button__BaseButton-sc-1qpsalo-0 haqezJ"]')
-            accept_cookies_button.click()
-            while time.time() < time_now + 5:
-                decline_ad = driver.find_element(by=By.XPATH, value='//*[@id="element-0JgZU8"]') 
-                decline_ad.click()
-        except AttributeError:
-            accept_cookies_button = driver.find_element(by=By.XPATH, value='//*[@id="button__BaseButton-sc-1qpsalo-0 haqezJ"]')
-            accept_cookies_button.click()
-            while time.time() < time_now + 5:
-                decline_ad = driver.find_element(by=By.XPATH, value='//*[@id="element-0JgZU8"]') 
-                decline_ad.click()
+            time.sleep(4)
+            driver.execute_script("window.scrollTo(0, 250)")
+            time.sleep(2)
+            decline_cookies_button = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="onetrust-reject-all-handler"]')))
+            decline_cookies_button.click()
+            
 
+        except TimeoutException:
+            print("Loading took too much time.")
+
+        try:
+            time.sleep(1)
+            driver.find_element(by=By.XPATH, value='//*[@id="tabs-:R2hj9aklt6:--tab-1"]').click()
+            
+            time.sleep(2)
+            driver.find_element(by=By.XPATH, value='//*[@href="/price"]').click()
+            time.sleep(3)
+            driver.execute_script("window.scrollTo(0, 500)")
+            time.sleep(2)
+            self.url_link_scraper()
+            
+            
         except:
             pass
+
+    def url_link_scraper(self):
+        links = []
+        links = links + driver.find_elements(by=By.TAG_NAME, value="a")
+        
+        for index in links[29:79]:
+            links = index.get_attribute("href")
+            print(links)
+            # scraper_links = links[30:79]
+        # print(scraper_links)
+
 
 run = ScraperClass(website)
 run.cookie_ad_clicker()
