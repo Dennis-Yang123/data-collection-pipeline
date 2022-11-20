0d333b2dd16817944558771c04f7ad25ac451684
Dennis Yang <dennisyang3.14@gmail.com>
2022-11-20 16:44:59 +0000

    Milestone 3 complete

--- scraper_class.py
+++ scraper_class.py
@@ -9,7 +9,7 @@ import time
 
 
 driver = webdriver.Chrome()
-URL = "https://crypto.com/price/ethereum"
+URL = "https://crypto.com/price"
 website = driver.get(URL)
 
 
@@ -21,29 +21,15 @@ class ScraperClass:
         delay = 10
         try:    
             time.sleep(4)
-            driver.execute_script("window.scrollTo(0, 250)")
+            driver.execute_script("window.scrollTo(0, 500)")
             time.sleep(2)
             decline_cookies_button = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="onetrust-reject-all-handler"]')))
             decline_cookies_button.click()
-            
+            self.url_link_scraper()
 
         except TimeoutException:
             print("Loading took too much time.")
 
-        try:
-            time.sleep(1)
-            driver.find_element(by=By.XPATH, value='//*[@id="tabs-:R2hj9aklt6:--tab-1"]').click()
-            
-            time.sleep(2)
-            driver.find_element(by=By.XPATH, value='//*[@href="/price"]').click()
-            time.sleep(3)
-            driver.execute_script("window.scrollTo(0, 500)")
-            time.sleep(2)
-            self.url_link_scraper()
-            
-            
-        except:
-            pass
 
     def url_link_scraper(self):
         links = []
@@ -54,8 +40,10 @@ class ScraperClass:
             print(links)
             # scraper_links = links[30:79]
         # print(scraper_links)
+        
+        return(links)
 
-
-run = ScraperClass(website)
-run.cookie_ad_clicker()
+if __name__ == '__main__':
+    run = ScraperClass(website)
+    run.cookie_ad_clicker()
 # %%
