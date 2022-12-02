9a96427712db978cc797f64f59b857d057e98853
Dennis Yang <dennisyang3.14@gmail.com>
2022-12-02 16:27:16 +0000

    Milestone 5 Task 5

---
+++ test_scraper_class.py
@@ -0,0 +1,20 @@
+# %%
+import unittest
+import scraper_class
+
+
+class TestScraperClass(unittest.TestCase):
+    def setUp(self):
+        self.scraper = scraper_class.ScraperClass
+
+    def test_cookie_ad_clicker(self):
+        self.assertEqual(self.scraper.cookie_ad_clicker(self), 1)
+
+    def test_url_link_scraper(self):
+        self.assertEqual(self.scraper.url_link_scraper(self), "https://crypto.com/price/bitcoin")
+       
+   
+if __name__ == "__main__":
+    unittest.main()
+
+# %%
